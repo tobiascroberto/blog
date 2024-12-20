6e78dc2b19e15efb6dd0e5683f9ceb76502c2126
Roberto Tobias <tobiasc.roberto@gmail.com>
2024-12-20 01:46:27 -0600

    cambiando el active nav

--- public/main.js
+++ public/main.js
@@ -1,12 +1,14 @@
-$(document).ready(function() {
-    $( ".mr-auto .nav-item" ).bind( "click", function(event) {
-        event.preventDefault();
-        var clickedItem = $( this );
-        $( ".mr-auto .nav-item" ).each( function() {
-            $( this ).removeClass( "active" );
-        });
-        clickedItem.addClass( "active" );
-    });
+console.log('hola desde footer', 2+2, pathname, $('.nav-item'))
+
+
+var pathname = window.location.pathname,
+pages = ['/', '/crear', '/nosotros'];
+
+$('.nav-item').each(function(i) {
+ if (pathname.includes(pages[i])) {
+    $(this).addClass('active')
+    console.log(pathname.includes(pages[i]))
+ }
+ else if ( $(this).className.includes('active'))  $(this).removeClass('active');
 });
 
-console.log('estoy en la pagina de')
