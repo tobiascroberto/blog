091aa2a8ce9810ba450997e8884d0cb8f3e8a3a3
Roberto Tobias <tobiasc.roberto@gmail.com>
2024-12-20 00:32:28 -0600

    modificado para usar en versel

--- api/index.js
+++ api/index.js
@@ -91,7 +91,10 @@ app.post('/calculadora',(req,res)=>{
     })
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
 app.listen(PORT, ()=>{
     console.log('Escuchando en puerto: '+ PORT)
 })
+
+
+module.exports = app;
