2cbfc5d8e144a0149f8d80672f062f7bafb67d48
Roberto Tobias <tobiasc.roberto@gmail.com>
2024-12-20 00:21:58 -0600

    creando blogs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const path = require('path')
 const bodyParser = require('body-parser')
+const { send } = require('process')
 
 const date = new Date()
 
@@ -18,14 +19,54 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine','ejs')
 
 
+// Entradas del blog
+
+let entradas = [
+    {
+        date: '3/12/2020',
+        title: 'Primera Entrada',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices augue quis mi sodales tristique. Donec laoreet odio nec metus pretium blandit. Proin sit amet.'
+    },
+    {
+        date: '3/11/2021',
+        title: 'Segunda Entrada',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices augue quis mi sodales tristique. Donec laoreet odio nec metus pretium blandit. Proin sit amet.'
+    },{
+        date: '3/09/2022',
+        title: 'Tercera Entrada',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices augue quis mi sodales tristique. Donec laoreet odio nec metus pretium blandit. Proin sit amet.'
+    },{
+        date: '3/07/2023',
+        title: 'Cuarta Entrada',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices augue quis mi sodales tristique. Donec laoreet odio nec metus pretium blandit. Proin sit amet.'
+    },
+]
+
+
+
 app.get('/',(req,res)=>{
-    res.render('pages/home')
+    res.render('pages/home', {todasLasEntradas : entradas})
 })
 
 
-app.get('/crear',(req,res)=>{
-    res.render('pages/crear')
+app.route('/crear')
+.get((req,res)=>{
+    res.render('pages/crear', {date: newDate})
 })
+.post((req,res)=>{
+    
+    console.log(req.body)
+    const date = req.body.date
+    const title = req.body.title
+    const content = req.body.content
+
+    entradas.push({date,title,content})
+
+    res.render('pages/crear', {date: newDate})
+
+})
+
+
 
 app.get('/nosotros',(req,res)=>{
     res.render('pages/nosotros')
