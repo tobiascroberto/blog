2f9a4bc0743642ae7e39035ba4ce501501957c3c
Roberto Tobias <tobiasc.roberto@gmail.com>
2024-12-21 00:02:59 -0600

    version para vercel con env

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
+
 const express = require('express')
 const morgan = require('morgan')
 const path = require('path')
 const bodyParser = require('body-parser')
 const { send } = require('process')
+const mongoose = require('mongoose')
 
 const date = new Date()
 
@@ -19,46 +22,126 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine','ejs')
 
 
+//conectar a base de datos mongodb
+mongoose.connect( process.env.BASEDEDATOS , {useNewUrlParser: true})
+
+
+// crear la base de datos en Mongoose
+// 1.- crear el Schema
+
+const entradaSchema = {
+    date:  String,
+    title: String,
+    content: String
+}
+
+// 2.- crear el modelo 
+
+const EntradaModelo = mongoose.model('Entrada',entradaSchema)
+
+
+// crud= create, read, updat y delete
+
+// const entradaUno = new EntradaModelo({
+//     date: '3/12/2020',
+//     title: 'Primera Entrada',
+//     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices augue quis mi sodales tristique. Donec laoreet odio nec metus pretium blandit. Proin sit amet.'
+// })
+
+// entradaUno.save()
+
+
+// para subir el resto de los registros: 
+
+// const entradaDos = new EntradaModelo({
+//     date: '3/11/2021',
+//     title: 'Segunda Entrada',
+//     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices augue quis mi sodales tristique. Donec laoreet odio nec metus pretium blandit. Proin sit amet.'
+// })
+
+// const entradaTres = new EntradaModelo({
+//     date: '3/09/2022',
+//     title: 'Tercera Entrada',
+//     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices augue quis mi sodales tristique. Donec laoreet odio nec metus pretium blandit. Proin sit amet.'
+// })
+
+// const arraySubir = [entradaDos, entradaTres]
+
+// EntradaModelo.insertMany(arraySubir)
+
+
+// READ en mongoDB
+//EntradaModelo.find().then(data => console.log(data))
+
+
+
 // Entradas del blog
 
-let entradas = [
-    {
-        date: '3/12/2020',
-        title: 'Primera Entrada',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices augue quis mi sodales tristique. Donec laoreet odio nec metus pretium blandit. Proin sit amet.'
-    },
-    {
-        date: '3/11/2021',
-        title: 'Segunda Entrada',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices augue quis mi sodales tristique. Donec laoreet odio nec metus pretium blandit. Proin sit amet.'
-    },{
-        date: '3/09/2022',
-        title: 'Tercera Entrada',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices augue quis mi sodales tristique. Donec laoreet odio nec metus pretium blandit. Proin sit amet.'
-    },{
-        date: '3/07/2023',
-        title: 'Cuarta Entrada',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices augue quis mi sodales tristique. Donec laoreet odio nec metus pretium blandit. Proin sit amet.'
-    },
-]
+// let entradas = [
+//     {
+//         date: '3/12/2020',
+//         title: 'Primera Entrada',
+//         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices augue quis mi sodales tristique. Donec laoreet odio nec metus pretium blandit. Proin sit amet.'
+//     },
+//     {
+//         date: '3/11/2021',
+//         title: 'Segunda Entrada',
+//         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices augue quis mi sodales tristique. Donec laoreet odio nec metus pretium blandit. Proin sit amet.'
+//     },{
+//         date: '3/09/2022',
+//         title: 'Tercera Entrada',
+//         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices augue quis mi sodales tristique. Donec laoreet odio nec metus pretium blandit. Proin sit amet.'
+//     },{
+//         date: '3/07/2023',
+//         title: 'Cuarta Entrada',
+//         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices augue quis mi sodales tristique. Donec laoreet odio nec metus pretium blandit. Proin sit amet.'
+//     },
+// ]
 
 
 
 app.get('/',(req,res)=>{
-    res.render('pages/home', {todasLasEntradas : entradas})
+
+
+    EntradaModelo.find().then( entradas => {
+
+
+    const entradasArrat = [] 
+    
+    entradas.forEach(items => {
+
+        entradasArrat.push({
+            title: items.title,
+            date: items.date,
+            content: items.content,
+            url: items.title.toLowerCase().split(' ').join('-')
+        })
+
+    })
+
+    res.render('pages/home', {todasLasEntradas : entradasArrat})
+})
+
 })
 
 
 app.route('/crear')
-.get((req,res)=>{
+.get((req,res)=>{ 
+    
     res.render('pages/crear', {date: newDate})
 })
 .post((req,res)=>{
     
+    const nuevaEntrada = new EntradaModelo(req.body)
 
-    entradas.push(req.body)
+    nuevaEntrada.save()
 
+    //version local
+    // entradas.push(req.body)
+
+    // res.render('pages/crear', {date: newDate})
     res.render('pages/crear', {date: newDate})
+
 
 })
 
@@ -86,6 +169,26 @@ app.post('/calculadora',(req,res)=>{
         num1, num2, resultado
     })
 })
+
+
+app.get('/entrada/:post', (req,res) => {
+
+    EntradaModelo.find().then( entradas => {
+        let resultado = {}
+        entradas.forEach(items =>{
+            if(req.params.post === items.title.toLowerCase().split(' ').join('-')){
+                resultado = {
+                    title: items.title,
+                    date: items.date,
+                    content: items.content
+                }
+            }
+        })
+
+        res.render('pages/pagina',{data: resultado})
+ })
+})
+
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, ()=>{
